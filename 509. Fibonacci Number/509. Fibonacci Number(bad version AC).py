9977f77505f5b7fa0872332791fdffa509ed8802
BohaoLiGithub <bli107@syr.edu>
2019-02-26 14:46:16 -0500

    Create 509. Fibonacci Number(bad version AC).py

---
+++ 509. Fibonacci Number/509. Fibonacci Number(bad version AC).py
@@ -0,0 +1,14 @@
+# exponential time
+
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 1:
+            return 1
+        elif N == 0:
+            return 0
+        else:
+            return self.fib(N-1) + self.fib(N-2)
